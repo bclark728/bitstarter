592c78e1353fb830ed0585fdff34664e6b676e3e
Bryan Clark <bryan.allan.clark@gmail.com>
2013-07-09 21:04:23 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,10 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = readFileSync('index.html');
-var msg = buf.toString();
-
 app.get('/', function(request, response) {
+  var buf = readFileSync('./index.html');
+  var msg = buf.toString();
   response.send(msg);
 });
 
